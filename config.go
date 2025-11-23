5032ce97476ab5e80735a023d74179e43561ddcc
zin-min-phyo-kozin <s6730042220@northcm.ac.th>
2025-11-23 16:00:26 +0700

    Create config.go with tool definitions and constants

---
+++ config.go
@@ -0,0 +1,12 @@
+// config.go
+
+package main
+
+// Tool definitions and constants
+const (
+	ToolName = "ghosts3c"
+	Version = "1.0.0"
+	Author = "zin-min-phyo-kozin"
+)
+
+// Add other relevant configurations and constants here
